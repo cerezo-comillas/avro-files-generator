0e17c9606f58081105ae4c750f9a4de14782d366
Felipe Cerezo <felipe.cerezo@hpe.com>
2023-12-28 15:46:32 +0100

    modified:   README.md modified:   dep.xml modified:   src/main/java/edu/comillas/bigdata/avro_generator/AvroGenerator.java

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # avro-files-generator
+
+To check avro files content online: https://zymeworks.github.io/avro-viewer/
--- src/main/java/edu/comillas/bigdata/avro_generator/AvroGenerator.java
+++ src/main/java/edu/comillas/bigdata/avro_generator/AvroGenerator.java
@@ -1,5 +1,7 @@
 package edu.comillas.bigdata.avro_generator;
 
+import com.fasterxml.jackson.annotation.JsonView;
+
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
